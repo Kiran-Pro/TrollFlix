adcb87a675df5f20bea9b24300e11cb4e06147c4
Kiran-Pro <krc5win@gmail.com>
2023-08-31 14:01:41 +0400

    changed the url

--- netflix_clone_js/src/request.js
+++ netflix_clone_js/src/request.js
@@ -1,6 +1,6 @@
 const API_KEY = "676612bb00a0e3cbd7c65ff9c8c6ef92";
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTrending: `movie/upcoming?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
